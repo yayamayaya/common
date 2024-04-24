cf150f33856f798472a7840a280af38338a05308
yaya <semenov.aleksandr@phystech.edu>
2024-04-24 17:23:54 +0300

    чтение файла в буфер

---
+++ file_reading/src/file_reading.cpp
@@ -0,0 +1,70 @@
+#include "file_reading.h"
+
+int get_file_size(FILE *file);
+
+int file_read(char **buff, int *file_size, const char *file_name)
+{
+    _INIT_LOG();
+    _OPEN_LOG("logs/file_reading.log");
+
+    int size_of_file = 0;
+    char *buffer = NULL;
+
+    LOG("> starting file reading:\n");
+    FILE *file_to_read = fopen(file_name, "rb");
+    if (!file_to_read)
+    {
+        LOG("[error]>>> couldn't open file to read, check the directory\n");
+        return READ_FILE_OPN_ERR;
+    }
+
+    size_of_file = get_file_size(file_to_read);
+    if (file_size == 0)
+    {
+        LOG("[error]>>> size of file is zero, check the file\n");
+        fclose(file_to_read);
+        _CLOSE_LOG();
+        return EMPTY_FILE_ERR;
+    }
+    LOG("> size of file is: %d\n", file_size);    
+        
+    LOG("> allocating memory:\n");
+    buffer = (char *)calloc(size_of_file + 1, sizeof(char));
+    if (!buffer)
+    {
+        LOG("[error]>>> couldn't allocate memory for buffer\n");
+        fclose(file_to_read);
+        _CLOSE_LOG();
+        return FILE_MEM_ALC_ERR;
+    }
+
+    int error = 0;
+    LOG("> memory alocated, reading file into the buffer\n");
+    if ((int)fread(buffer, size_of_file, sizeof(char), file_to_read) != size_of_file)
+    {
+        LOG("[error]>>> file wasn't read fully\n");
+        error = NOT_READ_FULLY;
+    }
+    LOG("> file reading completed");
+
+    *buff = buffer;
+    if (file_size)
+        *file_size = size_of_file;
+    
+    fclose(file_to_read);
+    _CLOSE_LOG();
+
+    return error;
+}
+
+int get_file_size(FILE *file)
+{
+    int current_position = ftell(file);
+
+    fseek(file, 0, SEEK_END);
+    int file_size = ftell(file);
+    fseek(file, current_position, SEEK_SET);
+
+    return file_size;
+}
+
