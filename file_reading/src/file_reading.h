15cc26b99954cf5c100c16e5a1e03ffa8606529a
yaya <semenov.aleksandr@phystech.edu>
2024-04-24 17:24:10 +0300

    чтение файла в буффер

---
+++ file_reading/src/file_reading.h
@@ -0,0 +1,18 @@
+#ifndef FILE_READING
+#define FILE_READING
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../logging/log.h"
+
+enum FILE_READING_ERRORS
+{
+    READ_FILE_OPN_ERR = 1313,
+    EMPTY_FILE_ERR,
+    FILE_MEM_ALC_ERR,
+    NOT_READ_FULLY,
+};
+
+int file_read(char **buff, int *file_size, const char *file_name);
+
+#endif
